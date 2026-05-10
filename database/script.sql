3e951fce87be451f0a09f8b457ddfdae6e542d1b
Guilherme Zambuzi <guilherme.hlz@puccampinas.edu.br>
2026-05-10 19:10:05 -0300

    Novo codigo banco

--- database/script.sql
+++ database/script.sql
@@ -1,45 +1,25 @@
-USE sistema_votacao;
+CREATE TABLE IF NOT EXISTS eleitores (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    cpf VARCHAR(50) NOT NULL UNIQUE,
+    titulo_eleitor VARCHAR(12) NOT NULL UNIQUE,
+    mesario BOOLEAN NOT NULL,
+    chave_acesso VARCHAR(50) NOT NULL,
+    status_voto VARCHAR(20) DEFAULT 'Não votou'
+);
 
 CREATE TABLE IF NOT EXISTS candidatos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
-    numero INT UNIQUE NOT NULL,
+    numero INT NOT NULL UNIQUE,
     partido VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS votos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_eleitor INT,
-    id_candidato INT,
-    data_hora DATETIME,
-    protocolo VARCHAR(100),
-
-    FOREIGN KEY (id_eleitor) REFERENCES eleitores(id),
+    id SERIAL PRIMARY KEY,
+    id_candidato INT NOT NULL,
+    data_hora TIMESTAMP NOT NULL,
+    protocolo VARCHAR(100) NOT NULL,
     FOREIGN KEY (id_candidato) REFERENCES candidatos(id)
 );
 
-CREATE TABLE IF NOT EXISTS urna (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    status VARCHAR(20),
-    data_abertura DATETIME,
-    data_encerramento DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data_hora DATETIME,
-    evento VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS protocolos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    codigo VARCHAR(100),
-    data_hora DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS resultados (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_candidato INT,
-    total_votos INT,
-    FOREIGN KEY (id_candidato) REFERENCES candidatos(id)
-);
